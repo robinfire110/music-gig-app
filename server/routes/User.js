595e90569dc158ea81036e4aa7ea028b7bd2e7b6
robinfire110 <aevillas@uncg.edu>
2024-03-03 21:25:09 -0500

    Added User API calls

--- server/routes/User.js
+++ server/routes/User.js
@@ -2,31 +2,127 @@ const express = require('express');
 const router = express.Router();
 const models = require('../database/models');
 
-//Get
+//Get all
+//Returns JSON of all users
 router.get("/", async (req, res) => {
+    try {
+        const users = await models.User.findAll({});
+        res.json(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
-    res.send("Get User - To be implemented. Ideally, you use /user to get all users and /user/user_id to get a specific user")
+//Get single by ID
+//Returns JSON of user with given user_id. Will be empty if does not exist.
+router.get("/id/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await models.User.findAll({where: {user_id: id}});
+        res.json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+//Get single by email
+//Returns JSON of user with given email. Will be empty if does not exist.
+router.get("/email/:email", async (req, res) => {
+    try {
+        const email = req.params.email;
+        const user = await models.User.findAll({where: {email: email}});
+        res.json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 //POST
+//Required - email, password, f_name, l_name, zip
+//Optional - Bio, is_admin (default: false), instrument(s)
+//Instrument can be either name or ID. Can mix and match.
 router.post("/", async (req, res) => {
-    const data = req.body;
+    try {
+        //Get data
+        const data = req.body;
 
-    //Post data
-    //Required - email, password, f_name, l_name, zip
-    //Optional - Bio, is_admin (defaults false), instrument(s)
-    await models.User.create({email: data?.email, password: data?.password, f_name: data?.f_name, l_name: data?.l_name, zip: data?.zip, bio: data?.bio, is_admin: data?.is_admin});
-    res.send(data);
+        //Add to User
+        const newUser = await models.User.create({email: data?.email, password: data?.password, f_name: data?.f_name, l_name: data?.l_name, zip: data?.zip, bio: data?.bio, is_admin: data?.is_admin});
+
+        //Add instrument (adds relation to UserInstrument table)
+        if (data.instruments)
+        {
+            for (const instrument of data.instruments) {
+                let instrumentId = instrument; 
+
+                //Get Instrument by name
+                if (typeof instrument == "string")
+                {
+                    instrumentId = (await models.Instrument.findOne({where: {name: instrument}}))?.instrument_id;
+                }
+                else
+                {
+                    instrumentId = (await models.Instrument.findOne({where: {instrument_id: instrument}}))?.instrument_id;
+                }
+                
+                //Add if found
+                if (instrumentId)
+                {
+                    await models.UserInstrument.create({instrument_id: instrumentId, user_id: newUser.user_id});
+                }
+                else
+                {
+                    console.log("Instrument not found. Possibly incorrect ID or name?. Skipping instrument");
+                }
+                
+            }
+        }
+        res.send(data);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+    
 });
 
-//PUT
-router.get("/", async (req, res) => {
-    res.send("Update User - Ideally would be /user/user_id with data to change specific user")
+//UPDATE
+//Can update fields. Only need to send the fields you wish to update
+router.put("/:id", async (req, res) => {
+    try {
+        const data = req.body;
+        const id = req.params.id;
+        const user = await models.User.findOne({where: {user_id: id}});
+        if (user)
+        {
+            user.set(data);
+            await user.save();
+            res.send(user);
+        }
+        else
+        {
+            res.status(404).send(`No user of user_id ${id} found.`);
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 //DELETE
-router.get("/", async (req, res) => {
-    res.send("Delete User - To be implemented. It would be /user/user_id to delete")
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await models.User.findOne({where: {user_id: id}});
+        if (user)
+        {
+            await user.destroy();
+            res.send(user);
+        }
+        else
+        {
+            res.status(404).send(`No user of user_id ${id} found.`);
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 //Export
