0580747f64ee58f28d183050989b70243354e743
Craig Smith <craigshmitty6@gmail.com>
2024-03-20 11:28:58 -0400

    added styling to events search page

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Container, Form, Col, Row, Button } from "react-bootstrap";
 import Header from "../components/Header";
+import "../styles/Events.css";
 
 const Events = () => {
 
@@ -67,20 +68,21 @@ const Events = () => {
                     <tbody>
                         {events.map((event) => (
                             <tr key={event.event_id}>
-                                <td>{formatDate(event.start_time)}</td>
-                                <td>{event.event_name}</td>
+                                <td className="venue-date"> <div className="key-item">{formatDate(event.start_time)}</div></td>
+                                <td> <div>{event.event_name}</div></td>
                                 <td>
                                     <div>
-                                        <div>Number of musicians: {event.Instruments && event.Instruments.length}</div>
+                                        <div>Number of musicians: <div className="key-item">{event.Instruments && event.Instruments.length}</div></div>
                                         <div>Instruments needed: </div>
                                         {event.Instruments && event.Instruments.map((instrument, index) => (
                                             <div className="key-item"> {instrument.name}</div>
                                         ))}
                                     </div>
                                 </td>
-                                <td>{event.Address && (
+                                <td className="venue-details">
+                                    {event.Address && (
                                     <div>
-                                        <div> <strong>Address:</strong> <br /> {event.Address.street} <br /> {event.Address.city} <br /> {event.Address.state} {event.Address.zip}</div>
+                                        <div> <strong>Address:</strong> {event.Address.street}, {event.Address.city} {event.Address.state}, {event.Address.zip}</div>
                                         <div> <strong>Time: </strong> {event.event_hours} hours </div>
                                         <div> <strong>Total Pay: </strong> ${event.pay} </div>
                                     </div>
