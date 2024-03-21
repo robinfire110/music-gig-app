5450360fc87649043e59412d0b8ad97eaf0f8aab
Craig Smith <craigshmitty6@gmail.com>
2024-03-21 10:48:21 -0400

    made EventRows component, added date range picker

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -4,14 +4,16 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Form, Col, Row, Button } from "react-bootstrap";
 import Header from "../components/Header";
+import EventRow from "../components/EventRow";
 import "../styles/Events.css";
 
 const Events = () => {
 
     const [events, setEvents] = useState([]);
-    let [filteredEvents, setFilteredEvents] = useState([]);
-    let [searchQuery, setSearchQuery] = useState("");
-    let [dateFilter, setDateFilter] = useState("");
+    const [filteredEvents, setFilteredEvents] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("")
 
     const navigate = useNavigate();
 
@@ -33,7 +35,10 @@ const Events = () => {
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        return date.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' })
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${month}/${day}/${year}`;
     }
 
     const goToEvent = (event_id) => {
@@ -42,18 +47,17 @@ const Events = () => {
 
     const handleSearch = (event) => {
         event.preventDefault();
-        let filteredEvents = events.filter(event => event.event_name.toLowerCase().includes(searchQuery.toLowerCase()));
+        let filteredEvents = events.filter(event => {
+            const eventDate = new Date(event.start_time);
+            return (!startDate || eventDate >= new Date(startDate)) && (!endDate || eventDate <= new Date(endDate))
+        });
         
-        if (dateFilter) {
-            filteredEvents = filteredEvents.filter(event => event.start_time.includes(dateFilter));
+        if (searchQuery) {
+            filteredEvents = filteredEvents.filter(event => event.event_name.toLowerCase().includes(searchQuery.toLowerCase()));
         }
         
         setFilteredEvents(filteredEvents);
     };
-
-    const handleDateFilter = (event) => {
-        setDateFilter(event.target.value);
-    }
 
     return (
         <div>
@@ -66,7 +70,10 @@ const Events = () => {
                             <Form.Control type="text" placeholder="Event Name" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
                         </Col>
                         <Col>
-                            <Form.Control type="date" placeholder="Date" value={dateFilter} onChange={handleDateFilter} />
+                            <Form.Control type="date" placeholder="Start Date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+                        </Col>
+                        <Col>
+                            <Form.Control type="date" placeholder="End Date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
                         </Col>
                         <Col>
                             <Button variant="primary" type="submit">Search</Button>
@@ -87,28 +94,7 @@ const Events = () => {
                     </thead>
                     <tbody>
                         {filteredEvents.map((event) => (
-                            <tr key={event.event_id}>
-                                <td className="venue-date"> <div className="key-item">{formatDate(event.start_time)}</div></td>
-                                <td> <div className="key-item">{event.event_name}</div></td>
-                                <td>
-                                    <div>
-                                        <div>Number of musicians: <div className="key-item">{event.Instruments && event.Instruments.length}</div></div>
-                                        <div>Instruments needed: </div>
-                                        {event.Instruments && event.Instruments.map((instrument, index) => (
-                                            <div className="key-item"> {instrument.name}</div>
-                                        ))}
-                                    </div>
-                                </td>
-                                <td className="venue-details">
-                                    {event.Address && (
-                                    <div>
-                                        <div> <strong>Address:</strong> {event.Address.street}, {event.Address.city} {event.Address.state}, {event.Address.zip}</div>
-                                        <div> <strong>Time: </strong> {event.event_hours} hours </div>
-                                        <div> <strong>Total Pay: </strong> ${event.pay} </div>
-                                    </div>
-                                )}</td>
-                                <td><Button onClick={() => goToEvent(event.event_id)}>View</Button></td>
-                            </tr>
+                            <EventRow key={event.event_id} event={event} goToEvent={goToEvent} formatDate={formatDate} />
                         ))}
                     </tbody>
                 </table>
