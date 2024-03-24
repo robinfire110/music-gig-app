e1a4ab64c004989dd8fe55adb3bd07cf48dc5fc3
Andy <andreabonola@gmail.com>
2024-03-24 19:05:33 -0400

    wip

--- client/src/pages/dashboards/Sidebar.jsx
+++ client/src/pages/dashboards/Sidebar.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import '../../App.css'
+import '../../App.css';
 
 function Sidebar({ handleLinkClick }) {
     return (
-        <Nav className="custom-sidebar flex-column">
-            <Nav.Link onClick={() => handleLinkClick('editProfile')} href="#">Profile</Nav.Link>
-            <Nav.Link onClick={() => handleLinkClick('gigs')} href="#">Gigs</Nav.Link>
-            <Nav.Link onClick={() => handleLinkClick('financials')} href="#">Financials</Nav.Link>
-        </Nav>
+        <div className="custom-sidebar-container">
+            <Nav className="custom-sidebar flex-column">
+                <Nav.Link
+                    onClick={() => handleLinkClick('editProfile')}
+                    href="#"
+                    className="custom-link"
+                >
+                    Profile
+                </Nav.Link>
+                <Nav.Link
+                    onClick={() => handleLinkClick('gigs')}
+                    href="#"
+                    className="custom-link"
+                >
+                    Gigs
+                </Nav.Link>
+                <Nav.Link
+                    onClick={() => handleLinkClick('financials')}
+                    href="#"
+                    className="custom-link"
+                >
+                    Financials
+                </Nav.Link>
+            </Nav>
+        </div>
     );
 }
 
 export default Sidebar;
+
 
