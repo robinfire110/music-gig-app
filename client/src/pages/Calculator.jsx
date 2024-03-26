35eb462ba106e88f3d98929b0297b7b3ac81ed15
Andy Villasmil <andyevillasmil@gmail.com>
2024-03-26 11:50:40 -0400

    Fixed account check issue with Calculator and Landing page

--- client/src/pages/Calculator.jsx
+++ client/src/pages/Calculator.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 import {BarLoader, ClipLoader} from 'react-spinners'
 import * as ExcelJS from "exceljs"
 import {saveAs} from "file-saver"
-import {formatCurrency, getCurrentUser, metersToMiles, parseFloatZero, parseIntZero} from "../Utils";
+import {autoSizeColumn, formatCurrency, getCurrentUser, metersToMiles, parseFloatZero, parseIntZero} from "../Utils";
 import { useCookies } from "react-cookie";
 import { toast } from "react-toastify";
 
@@ -98,37 +98,36 @@ const Calculator = () => {
         }
 
         //Get user
-        axios.get('http://localhost:5000/account', {withCredentials: true}).then(res => {
-            if (res.data?.user)
-            {
-                const userData = res.data.user;
-                setModalOriginZip(userData.zip);
-                setUser(userData);
-
-                //Load data
-                console.log(user);
-                if (paramId)
+        if (cookies.jwt)
+        {
+            axios.get('http://localhost:5000/account', {withCredentials: true}).then(res => {
+                if (res.data?.user)
                 {
-                    loadFromDatabase(userData).then(() => {
-                        setIsLoading(false);
-                    });
-                } 
-                else setIsLoading(false);
-            }
-            else 
-            {
-                setUser(undefined);
-                if (paramId)
+                    const userData = res.data.user;
+                    setModalOriginZip(userData.zip);
+                    setUser(userData);
+                }
+                else 
                 {
-                    loadFromDatabase().then(() => {
-                        setIsLoading(false);
-                    });
-                } 
-                else setIsLoading(false);
-            }
-            console.log("User Data", res.data);
-        });
+                    setUser(undefined);
+                }
+                console.log("User Data", res.data);
+            });
+        }
+        
     }, []);
+
+    //Load from database
+    useEffect(() => {
+        console.log("Loaded", user);
+        if (paramId)
+        {
+            loadFromDatabase().then(() => {
+                setIsLoading(false);
+            });
+        } 
+        else setIsLoading(false);
+    }, [user])
     
     //Runs when any fields related to calculation updates.
     useEffect(() => {
@@ -255,7 +254,7 @@ const Calculator = () => {
             }
             else
             {
-                console.log("NO EVENT FOUND")
+                console.log("No event found");
             }
             }).catch(error => {
                 console.log(error)
@@ -495,95 +494,7 @@ const Calculator = () => {
         }
     }
 
-    //Auto fit column width
-    function autoSizeColumn(worksheet)
-    {
-        worksheet.columns.forEach(function (column, i) {
-            let maxLength = 0;
-            column["eachCell"]({ includeEmpty: true }, function (cell) {
-                var columnLength = cell.value ? cell.value.toString().length : 10;
-                if (columnLength > maxLength ) {
-                    maxLength = columnLength;
-                }
-            });
-            column.width = maxLength;
-        });
-    }
-
-    async function saveSpreadsheetAll()
-    {
-        await axios.get(`http://localhost:5000/financial/user_id/${user?.user_id}`).then(async res => {
-            const data = res.data;
-            //Create workbook
-            const workbook = new ExcelJS.Workbook();
-            workbook.created = new Date();
-            workbook.modified = new Date();
-
-            //Create worksheet
-            const worksheet = workbook.addWorksheet("Calculator Results", {
-                views: [{ state: "frozen", ySplit: 1 }]
-                
-            });
-
-            //Fomatting
-            const moneyColumns = ["C", "J", "L", "O", "P", "Q", "S"];
-            moneyColumns.forEach(column => {
-                worksheet.getColumn(column).numFmt = '$#,##0.00';
-            });
-            worksheet.getColumn("N").numFmt = '0.00##\\%'; //Format percen
-            worksheet.getColumn("S").font = {bold: true};
-
-            //Set header
-            const headerRow = ["Name", "Date", "Total Wage", "# of Gigs", "Event Hours", "Practice Hours", "Rehearsal Hours", "Total Mileage", "Travel Hours", "Gas $/Gallon", "Vehicle MPG", "Gas $/Mile", "Mileage Covered", "Tax %", "Other Fees", "Tax Cut", "Travel Cost", "Total Hours", "Hourly Wage"]
-            worksheet.addRow(headerRow).commit();
-            worksheet.getRow(1).font = {bold: true};
-            worksheet.getRow(1).numFmt = "";
-            worksheet.getCell(`P1`).border = {left: {style: "thin"}};
-
-            //Size columns
-            autoSizeColumn(worksheet);
-            worksheet.getColumn("A").width = 20; //Name width
-            worksheet.getColumn("B").width = 11; //Date width
-            worksheet.getColumn("N").width = 8; //Tax width
-
-            //Set data
-            let rowCount = 1;
-            data.forEach(fin => {
-                rowCount++;
-                var row = worksheet.addRow([fin.fin_name, fin.date, parseFloatZero(fin.total_wage), parseIntZero(gigNum) == 0 ? 1 : parseIntZero(gigNum), parseFloatZero(fin.event_hours), parseFloatZero(fin.practice_hours), parseFloatZero(fin.rehearse_hours), parseFloatZero(fin.total_mileage), parseFloatZero(fin.travel_hours), parseFloatZero(fin.gas_price), parseFloatZero(fin.mpg), parseFloatZero(fin.gas_price/fin.mpg), parseFloatZero(fin.mileage_pay), parseFloatZero(fin.tax), parseFloatZero(fin.fees), fin.total_wage*(.01*parseFloatZero(fin.tax)), parseFloatZero(fin.gas_price/fin.mpg)*parseFloatZero(fin.total_mileage), 0, parseFloatZero(fin.hourly_wage)]);
-            
-                //Set Formulas
-                worksheet.getCell(`L${rowCount}`).value = {formula: `IFERROR(J${rowCount}/K${rowCount}, 0)`}; //Gas Price Per Mile
-                worksheet.getCell(`P${rowCount}`).value = {formula: `(C${rowCount}*D${rowCount})*(0.01*N${rowCount})`}; //Tax Cut
-                worksheet.getCell(`Q${rowCount}`).value = {formula: `H${rowCount}*(L${rowCount}-M${rowCount})`}; //Travel Costs
-                worksheet.getCell(`R${rowCount}`).value = {formula: `((E${rowCount}*D${rowCount})+F${rowCount}+G${rowCount}+I${rowCount})`}; //Total Hours
-                worksheet.getCell(`S${rowCount}`).value = {formula: `IFERROR(((C${rowCount}*D${rowCount})-O${rowCount}-P${rowCount}-Q${rowCount})/R${rowCount}, 0)`}; //Total Hourly Wage
-                
-                //Border
-                worksheet.getCell(`P${rowCount}`).border = {left: {style: "thin"}};
-                row.commit();
-            });
-
-            //Final sum
-            worksheet.getCell(`O${rowCount+2}`).value = "Total";
-            worksheet.getCell(`O${rowCount+2}`).border = {top: {style: "medium"}};
-            const sumRows = ["P", "Q", "R", "S"];
-            sumRows.forEach(row => {
-                var cell = worksheet.getCell(`${row}${rowCount+2}`);
-                cell.value = {formula: `SUM(${row}2:${row}${rowCount})`};
-                cell.border = {top: {style: "medium"}};
-            });
-            worksheet.getRow(rowCount+2).font = {bold: true};
-
-            //Save
-            const buf = await workbook.xlsx.writeBuffer();
-            saveAs(new Blob([buf]), `Harmonize_Export.xlsx`);
-
-        }).catch(error => {
-            console.log(error)
-        })
-    }
-
+    //Save spreadsheet
     async function saveSpreadsheet()
     {
         try 
