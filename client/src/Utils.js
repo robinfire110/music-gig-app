63410fda295a8c45330233901341c73099228ba2
Andy Villasmil <100443547+robinfire110@users.noreply.github.com>
2024-04-03 22:09:32 -0400

    CORS *

--- client/src/Utils.js
+++ client/src/Utils.js
@@ -6,6 +6,7 @@ let REACT_APP_BACKEND_URL;
 
 function getBackendURL()
 {
+    return "/";
     if (window.location.hostname == "localhost") return "localhost:5000";
     else return "/";
 }
--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const port = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["http://localhost:3000", "https://harmonize.rocks"],
+    origin: "*",
     method: ["GET", "POST"],
     credentials: true
 }));
