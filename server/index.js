516dd38e18ce61642ce258421f9946fa0773481d
Andy Villasmil <andyevillasmil@gmail.com>
2024-04-05 17:42:50 -0400

    Added CORS

--- server/index.js
+++ server/index.js
@@ -35,6 +35,8 @@ if (process.env.NODE_ENV === "development")
 else if (process.env.NODE_ENV === "production")
 {
     allowedOrigins.push("https://harmonize.rocks");
+    allowedOrigins.push("http://harmonize.rocks");
+    allowedOrigins.push("http://http://152.70.204.132/");
 }
 
 // Middleware setup
