44ec86ede0ca93d0a3a04331829ee57cf7dfca5f
Andy Villasmil <andyevillasmil@gmail.com>
2024-04-05 17:46:19 -0400

    Set back API

--- client/src/Utils.js
+++ client/src/Utils.js
@@ -3,7 +3,8 @@ const { Axios } = require("axios");
 function getBackendURL()
 {
     if (process.env.NODE_ENV== "development") return "http://localhost:5000";
-    else return "https://harmonize.rocks/api";
+    else return "api";
+    //else return "https://harmonize.rocks/api";
 }
 
 //Constant Variables
