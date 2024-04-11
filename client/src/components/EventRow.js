1f708cb7e069c3106f262794cb8538e47cffcdfd
Andy Villasmil <100443547+robinfire110@users.noreply.github.com>
2024-04-11 18:53:15 -0400

    Updated Event Search
    Updated layout
    Added sort by

--- client/src/components/EventRow.js
+++ client/src/components/EventRow.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button, Row, Col, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { getEventOwner } from "../Utils";
 
 const EventRow = ({ event, goToEvent, formatDate, index }) => {
     let backgroundColor = "rgba(255, 255, 255, 1)"
     if (index % 2 != 0) backgroundColor = "rgba(100, 100, 100, .15)";
+    const owner = getEventOwner(event);
 
     //Instruments
     const instrumentNames = event.Instruments.map((instrument) => {
@@ -18,11 +20,12 @@ const EventRow = ({ event, goToEvent, formatDate, index }) => {
                 <Col lg={1}><div>{formatDate(event.start_time)}</div></Col>
                 <Col lg={2}><div>{event.event_name}</div></Col>
                 <Col lg={1}><div>${event.pay}</div></Col>
-                <Col lg={3}><div>{instrumentNames.length > 0 ? instrumentNames.join(", ") : "No instruments provided"}</div></Col>
-                <Col lg={4}><div>
+                <Col lg={2}><div>{instrumentNames.length > 0 ? instrumentNames.join(", ") : <div className="text-muted">No instruments provided</div>}</div></Col>
+                <Col lg={2}><div>{owner?.f_name} {owner?.l_name}</div></Col>
+                <Col lg={3}><div>
                         {event.Address && (
                             <div>
-                                <div> <strong>Address:</strong> {event.Address.street}, {event.Address.city} {event.Address.state}, {event.Address.zip}</div>
+                                {event.Address.street},<br />{event.Address.city} {event.Address.state}, {event.Address.zip}
                             </div>
                         )}
                     </div></Col>
@@ -34,27 +37,3 @@ const EventRow = ({ event, goToEvent, formatDate, index }) => {
 }
 
 export default EventRow;
-
-{/*
-        <tr>
-            <td className="venue-date"> <div>{formatDate(event.start_time)}</div></td>
-            <td> <div>{event.event_name}</div></td>
-            <td>
-                <div>
-                    <div>Instruments needed: </div>
-                    {event.Instruments && event.Instruments.map((instrument, index) => (
-                        <div key={index}> {instrument.name}</div>
-                    ))}
-                </div>
-            </td>
-            <td className="venue-details">
-                {event.Address && (
-                    <div>
-                        <div> <strong>Address:</strong> {event.Address.street}, {event.Address.city} {event.Address.state}, {event.Address.zip}</div>
-                        <div> <strong>Time: </strong> {event.event_hours} hours </div>
-                        <div> <strong>Total Pay: </strong> ${event.pay} </div>
-                    </div>
-                )}</td>
-            <td><Button onClick={() => goToEvent(event.event_id)}>View</Button></td>
-        </tr>
-        */}
