4507a624561ad56a8f527e90fbb5f6620a622521
Andy Villasmil <100443547+robinfire110@users.noreply.github.com>
2024-04-13 10:36:48 -0400

    Added 404 Page

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return (
+        <Container className="text-center">
+            <br />
+            <h1>Page Not Found</h1>
+            <br />
+            <br />
+            <h4>Apologies, we can't find the page you are looking for.</h4>
+            <br />
+            <br />
+            <h4>Please ensure you have the correct URL or click <Link to="/">here</Link> to return to the home page.</h4>
+            <br />
+        </Container>
+    )
+}
+
+export default NotFound
